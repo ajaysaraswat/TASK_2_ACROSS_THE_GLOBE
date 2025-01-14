90c1cb618608893bd2456f95b9aa60eac174818b
AJAY KUMAR SARASWAT <ajaykumarsaraswat9045@gmail.com>
2025-01-15 00:41:00 +0530

    making this real time

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const coinRouter = require("./routes/coin");
 const path = require("path");
+const http = require("http");
+const { Server } = require("socket.io");
 const app = express();
 const PORT = 8000;
+
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static("./views"));
 app.set("view engine", "ejs");
@@ -10,6 +15,6 @@ app.set("views", path.resolve("./views"));
 
 app.use("/", coinRouter);
 
-app.listen(PORT, (req, res) => {
+server.listen(PORT, (req, res) => {
 	console.log(`server is running on port ${PORT}`);
 });
