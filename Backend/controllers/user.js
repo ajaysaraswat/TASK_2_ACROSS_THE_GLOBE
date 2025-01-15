a73c9c78d9de4fe46f4ff2cef06a6c1bfae18afa
AJAY KUMAR SARASWAT <ajaykumarsaraswat9045@gmail.com>
2025-01-15 22:57:44 +0530

    user creation

---
+++ Backend/controllers/user.js
@@ -0,0 +1,19 @@
+const User = require("../models/user");
+
+const handlepostuser = async (req, res) => {
+	try {
+		const body = req.body;
+		if (!body) return res.status(400).send({ message: "invalid body" });
+		const user = await User.create({
+			fullname: body.fullname,
+			email: body.email,
+			password: body.password,
+		});
+
+		return res
+			.status(201)
+			.json({ status: "Created Successfully", message: user._id });
+	} catch (err) {
+		return res.redirect("/");
+	}
+};
---
+++ Backend/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+	{
+		fullname: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		alerts: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Alert",
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
